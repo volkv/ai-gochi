9f0c66d5c869c0887e5ca927a0c68bf7b6074322
volkv <pvolkv@gmail.com>
2025-08-23 13:18:28 +0300

    wip

---
+++ app/src/main/java/com/hackathon/echo/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.hackathon.echo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hackathon.echo.ui.theme.EchoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            EchoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    EchoTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/hackathon/echo/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package com.hackathon.echo.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// Pet emotion colors
+val Joy = Color(0xFFFFD700)
+val Sadness = Color(0xFF4169E1) 
+val Thoughtful = Color(0xFF9370DB)
+val Calm = Color(0xFF32CD32)
+val Neutral = Color(0xFF808080)
